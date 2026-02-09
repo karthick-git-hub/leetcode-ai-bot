6dcaceb70fb87c0f64aa228bce7f1d02aae567b2
Karthick Anbalagan <70504841+karthick-git-hub@users.noreply.github.com>
2026-02-09 18:41:44 -0500

    Add solution for 1382. balance-a-binary-search-tree

--- solutions/1382-balance-a-binary-search-tree.java
+++ solutions/1382-balance-a-binary-search-tree.java
@@ -1,27 +1,50 @@
-import java.util.*;
-
-public class Solution {
+class Solution {
     public TreeNode balanceBST(TreeNode root) {
         List<Integer> vals = new ArrayList<>();
-        inorder(root, vals);
+        inOrder(root, vals);
         
-        return buildBalancedBST(vals);
+        int start = 0;
+        int end = vals.size() - 1;
+        
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            TreeNode node = new TreeNode(vals.get(mid));
+            
+            node.left = balanceBST(rootLeft(vals, start, mid));
+            node.right = balanceBST(rootRight(vals, mid + 1, end));
+            
+            return node;
+        }
+        
+        return null;
     }
     
-    private void inorder(TreeNode node, List<Integer> vals) {
-        if (node == null)
-            return;
-        inorder(node.left, vals);
-        vals.add(node.val);
-        inorder(node.right, vals);
-    }
-    
-    private TreeNode buildBalancedBST(List<Integer> vals) {
-        if (vals.isEmpty())
+    private TreeNode rootLeft(List<Integer> vals, int start, int end) {
+        if (start > end)
             return null;
         
-        int mid = vals.size() / 2;
-        TreeNode root = new TreeNode(vals.get(mid));
-        root.left = buildBalancedBST(Arrays.copyOfRange(vals, 0, mid));
-        root.right = buildBalancedBST(Arrays.copyOfRange(vals, mid + 1, vals.size()));
-        return root;
+        TreeNode node = new TreeNode(vals.get(start));
+        start++;
+        
+        return node;
+    }
+    
+    private TreeNode rootRight(List<Integer> vals, int start, int end) {
+        if (end < start)
+            return null;
+        
+        TreeNode node = new TreeNode(vals.get(end));
+        end--;
+        
+        return node;
+    }
+    
+    private void inOrder(TreeNode node, List<Integer> vals) {
+        if (node == null)
+            return;
+        
+        inOrder(node.left, vals);
+        vals.add(node.val);
+        inOrder(node.right, vals);
+    }
+}
