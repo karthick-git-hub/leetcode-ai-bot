a0582dedf77309d196583327df03a64e11447eeb
Karthick Anbalagan <70504841+karthick-git-hub@users.noreply.github.com>
2026-02-09 20:03:31 -0500

    Add solution for 3719. longest-balanced-subarray-i

--- solutions/3719-longest-balanced-subarray-i.java
+++ solutions/3719-longest-balanced-subarray-i.java
@@ -1,39 +1,56 @@
-import java.util.*;
-
 public class Solution {
-    public int longestSubarray(int[] nums) {
-        Set<Integer> evens = new HashSet<>();
-        Set<Integer> odds = new HashSet<>();
-
+    public int findLongestSubarray(int[] nums) {
         int maxLen = 0;
-        int curLen = 0;
+        int n = nums.length;
+        int lastEven = -1; // stores the index of the last seen even number in evens
+        int lastOdd = -1;   // stores the index of the last seen odd number in odds
 
-        for (int num : nums) {
-            if ((num & 1) != 0) {
-                odds.add(num);
-            } else {
-                evens.add(num);
-            }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] % 2 == 0) {
+                nums[i] %= 2;
+                while (lastEven != -1 && evens[lastEven] != nums[i]) {
+                    lastEven = evens[lastEven];
+                }
+                lastEven = Math.max(lastEven, i);
 
-            while (!evens.isEmpty() && !odds.isEmpty()) {
-                if (((nums[(int) evens.iterator().next()] & 1) == 0)) {
-                    evens.remove(nums[(int) evens.iterator().next()]);
+                int j = i + 1;
+
+                if (lastOdd == -1 || evens[lastEven] == nums[j % n]) {
+                    while (lastOdd != -1 && odds[lastOdd] != nums[(j-1) % n]) {
+                        lastOdd = odds[lastOdd];
+                    }
+                    lastOdd = Math.max(lastOdd, i);
                 } else {
-                    odds.remove(nums[(int) odds.iterator().next()]);
+                    maxLen = Math.max(maxLen, i - lastEven + 1);
+                    j++;
                 }
 
-                curLen++;
-            }
-
-            maxLen = Math.max(maxLen, curLen);
-
-            if ((num & 1) != 0) {
-                odds.add(num);
+                evens[i] = nums[i];
             } else {
-                evens.add(num);
+                while (lastEven != -1 && odds[lastOdd] != nums[i]) {
+                    lastOdd = odds[lastOdd];
+                }
+                lastOdd = Math.max(lastOdd, i);
+
+                int j = i + 1;
+
+                if (lastEven == -1 || odds[lastOdd] == nums[j % n]) {
+                    while (lastEven != -1 && evens[lastEven] != nums[(j-1) % n]) {
+                        lastEven = evens[lastEven];
+                    }
+                    lastEven = Math.max(lastEven, i);
+                } else {
+                    maxLen = Math.max(maxLen, i - lastOdd + 1);
+                    j++;
+                }
+
+                odds[i] = nums[i];
             }
         }
-
+        
         return maxLen;
     }
+
+    private final int[] evens = new int[1500];
+    private final int[] odds   = new int[1500];
 }
