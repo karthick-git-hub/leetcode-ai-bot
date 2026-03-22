1e4bcd994605df265de6309fcb4f7d3825c826d3
Karthick Anbalagan <70504841+karthick-git-hub@users.noreply.github.com>
2026-03-22 19:30:41 +0530

    Add solution for 1886. determine-whether-matrix-can-be-obtained-by-rotation

--- solutions/1886-determine-whether-matrix-can-be-obtained-by-rotation.java
+++ solutions/1886-determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -1,27 +1,32 @@
+import java.util.*;
+
 class Solution {
     public boolean findRotation(int[][] mat, int[][] target) {
         int n = mat.length;
-        for (int rot = 0; rot < 4; rot++) {
-            boolean ok = true;
-            for (int i = 0; i < n && ok; i++) {
+        for (int r = 0; r < 4; r++) {
+            boolean match = true;
+            for (int i = 0; i < n && match; i++) {
                 for (int j = 0; j < n; j++) {
                     int val;
-                    if (rot == 0) {
+                    if (r == 0) {
                         val = mat[i][j];
-                    } else if (rot == 1) { // 90 degrees
+                    } else if (r == 1) {
+                        // 90 degrees clockwise
                         val = mat[n - 1 - j][i];
-                    } else if (rot == 2) { // 180 degrees
+                    } else if (r == 2) {
+                        // 180 degrees
                         val = mat[n - 1 - i][n - 1 - j];
-                    } else { // 270 degrees
+                    } else {
+                        // 270 degrees clockwise
                         val = mat[j][n - 1 - i];
                     }
                     if (val != target[i][j]) {
-                        ok = false;
+                        match = false;
                         break;
                     }
                 }
             }
-            if (ok) return true;
+            if (match) return true;
         }
         return false;
     }
